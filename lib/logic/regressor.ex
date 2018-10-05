6ba4296ca64b001f02a4fb407d7cbd7bca1452d6
yukq16 <shikine.yui@gmail.com>
2018-10-05 17:09:01 +0900

    Added mult() for matrix 2dim

--- lib/logic/regressor.ex
+++ lib/logic/regressor.ex
@@ -1,117 +1,54 @@
 defmodule NifRegressor do
 	use Rustler, otp_app: :linear_regressor_cli, crate: :regressor
 
-	@index 10_000_000
+	@doc """
+ 	## Examples
+	
+	"""
 
 	# For List Function
 	def dot_product(_a, _b), do: exit(:nif_not_loaded)
-	def nif_zeros(_a), do: exit(:nif_not_loaded)
-	def nif_new(_a, _b), do: exit(:nif_not_loaded)
+	def zeros(_a), do: exit(:nif_not_loaded)
+	def new(_a, _b), do: exit(:nif_not_loaded)
 	def sub(_a, _b), do: exit(:nif_not_loaded)
+	def emult(_a, _b), do: exit(:nif_not_loaded)
 
-	def zeros(n) when n < 1, do: []
-	def zeros(n) do
-		[0] ++ zeros(n-1)
+	# Main Function
+	def fit(_x, _y, _theta, _alpha, _iteration), do: exit(:nif_not_loaded)
+	def test(_x, _y), do: exit(:nif_not_loaded)
+
+	# Sub Function
+	# def elem()
+	# def dot_product(r1, _r2) when r1 == [],do: 0
+	# def dot_product(r1, r2), do: nif_dot_product(r1, r2)
+
+	def fit_with_nif( x, y, theta, alpha, iterations ) do
+		m = length( y )
+        tx = Matrix.transpose( x )
+        size = Matrix.size( theta )
+        a = Matrix.new( elem( size, 0 ), elem( size, 1 ), alpha * ( 1 / m ) )
+
+        0..iterations
+        |> Enum.to_list
+        |> Enum.reduce( theta, fn( _iteration, theta ) ->
+            trans_theta = Matrix.transpose( theta )
+            d = Enum.map(x, fn(row)->
+            		Enum.map(trans_theta, &dot_product(row, &1))
+            	end)
+            d =	Enum.zip(d, y)
+            |> Enum.map(fn({a,b})->sub(a,b) end)
+
+            d = Matrix.transpose( d )
+            d = Enum.map(d, fn(row) ->
+            		Enum.map(tx, &dot_product(row, &1))
+            	end)
+            d = Matrix.transpose( d )
+            d = Enum.zip(d, a)
+            	|> Enum.map( fn({a, b})->emult(a,b) end)
+            Enum.zip(theta, d)
+            |> Enum.map( fn({a,b})->sub(a,b) end)
+        end )
 	end
 
-	def new(e1, e2) when e1 > e2, do: []
-	def new(e1, e2) do
-		[e1] ++ new(e1+1, e2)
-	end
-
-	# Benchmark
-	
-	# tool
-	def timer( f ) do
-		:timer.tc( fn -> f.() end)
-		|> case do
-			{ elapsed, res } -> IO.inspect( elapsed / 1_000_000 )
-			res
-		end
-	end
-
-	### make list
-	# later
-	def new_list_benchmark do
-		:timer.tc( fn ->
-			1..@index
-			|> Enum.to_list
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end
-
-	# faster
-	def new_list_benchmark1 do
-		:timer.tc( fn ->
-			new(1, @index) 
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end
-
-	### zeros 
-	# very fast
-	def zeros_benchmark do
-		:timer.tc( fn ->
-			List.duplicate(0, @index)
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end 
-
-	# later 
-	def zeros_benchmark1 do
-		:timer.tc( fn ->
-			zeros(@index)
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end
-
-	# fast
-	def zeros_benchmark2 do
-		:timer.tc( fn ->
-			nif_zeros(@index)
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end
-
-	def dp_benchmark do
-		m = nif_new(1, @index) |>elem(1)
-		:timer.tc( fn ->
-			dot_product_ex(m, m) 
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end
-
-	def dp_benchmark1 do
-		m = nif_new(1, @index) |>elem(1)
-		:timer.tc( fn ->
-			dot_product(m, m)
-		end)
-		|>elem(0)
-		|>Kernel./(1_000_000)
-	end
-
-	@doc """
- 	dot_product.
-
-	## Examples
-
-	iex> LinearRegressorNif.dot_product_ex([1, 2, 3], [4, 5, 6])
-	32
-
-	iex> LinearRegressorNif.dot_product_ex([1.0, 2.0, 3.0], [4.0, 5.0, 6.0])
-	32.0
-
-	"""
-	def dot_product_ex(r1, _r2) when r1 == [], do: 0
-	def dot_product_ex(r1, r2) do
-		[h1|t1] = r1
-		[h2|t2] = r2
-		(h1*h2) + dot_product_ex(t1, t2)
-	end
+	# end 
 end
--- native/regressor/src/lib.rs
+++ native/regressor/src/lib.rs
@@ -2,8 +2,6 @@
 // #[macro_use] extern crate rustler_codegen;
 #[macro_use] extern crate lazy_static;
 
-
-
 use rustler::{Env, Term, NifResult, Encoder};
 // use rustler::env::{OwnedEnv, SavedTerm};
 // use rustler::types::atom::Atom;
@@ -17,11 +15,12 @@ use rustler::{Env, Term, NifResult, Encoder};
 // use rayon::prelude::*;
 
 // type NifResult<T> = Result<T, Error>;
+type Num = i64;
 
 mod atoms {
     rustler_atoms! {
         atom ok;
-        //atom error;
+        atom error;
         //atom __true__ = "true";
         //atom __false__ = "false";
     }
@@ -35,52 +34,151 @@ rustler_export_nifs! {
     "Elixir.NifRegressor",
     [
         //("Elixir's func, number of arguments, Rust's func)
-        ("dot_product", 2, dot_product),
-        ("nif_zeros", 1, zeros),
-        ("nif_new", 2, new),
-        ("sub", 2, sub)
+        ("dot_product", 2, nif_dot_product),
+        ("zeros", 1, zeros),
+        ("new", 2, new),
+        ("sub", 2, nif_sub),
+        ("emult", 2, nif_emult),
+        ("fit", 5, nif_fit), 
+        ("test", 2, test)
     ],
     None
 }
 
-fn dot_product<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
-    // Initialize Arguments
-    // Decode to Vector
-    let x: Vec<f64> = args[0].decode()?;
-    let y: Vec<f64> = args[1].decode()?;
-    
+pub fn dot_product(x: Vec<Num>, y: Vec<Num>) -> Num {
     // Main Process 
     let tuple = x.iter().zip(y.iter());
-    let mut ans = 0.0;
-    for (i, j) in tuple{
-        ans = ans + (i*j);
-    }
-
-    // Return
-    Ok((atoms::ok(), ans).encode(env))
+    tuple.map(|t| t.0 * t.1).fold(0, |sum, i| sum +i)
 }
 
-fn sub<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
-    let x: Vec<f64> = args[0].decode()?;
-    let y: Vec<f64> = args[1].decode()?;
+pub fn sub(x: Vec<Num>, y: Vec<Num>) -> Vec<Num> {
+    x.iter().zip(y.iter())
+    .map(|t| t.0 - t.1)
+    .collect()
+}
+
+pub fn emult(x: Vec<Num>, y: Vec<Num>) -> Vec<Num> {
+    x.iter().zip(y.iter())
+    .map(|t| t.0 * t.1)
+    .collect()
+
+}
+
+pub struct Matrix {
+    data: Vec<Num>, 
+    row_size: usize,
+    col_size: usize,
+    transpose: bool,
+}
+
+impl Matrix{
+  pub fn data(&self) -> Vec<Num> {
+    self.data.clone()
+  }
+}
+
+// pub fn new_matrix(x: Vec<Vec<Num>>, init: Num) -> Matrix{
+//     let row = x.len();
+//     let col = x[0].len();    
+// }
+
+pub fn transpose(x: Vec<Vec<Num>>) -> Matrix {
+
+}
+
+pub fn mult2d(x: Vec<Vec<Num>>, y: Vec<Vec<Num>>) -> Matrix {
+  let ans_row = x[0].len();
+  let ans_col = y[0].len();
+
+  let mut ans: Vec<Num> = Vec::with_capacity(ans_row*ans_col);
+  let mut sum = 0;
+
+  println!("ans_row:{:?}", ans_row);
+  println!("ans_col:{:?}", ans_col);
+
+  for i in 0..x.len(){
+    for k in 0..ans_col {
+      for j in 0..ans_row{    
+        sum = sum + x[i][j]*y[j][k];
+      }            // println!("{:?}", sum);
+      ans.push(sum);
+      sum = 0;
+    }        
+  }
+
+  Matrix{
+      data: ans,
+      row_size: ans_row,
+      col_size: ans_col,
+      transpose: false,
+  }
+}
+
+// pub fn transpose (x: Vec<Vec<i64>>) -> Vec<Vec<i64>> {
+//     let x
+
+//     let mut ans = x.clone();
+
+//     let tuple = x.iter()
+//     ans.reverse();
+//     ans
+// }
+
+fn test<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
+    let mut x: Vec<Vec<Num>> = args[0].decode()?;
+    let y: Vec<Vec<Num>> = args[1].decode()?;
+
+    // if x[0].len() != y.len(){
+    //     Err(atoms::error().encode(env));
+    // }
+
+    for i in 0..x.len(){
+        for j in 0..x[0].len(){
+            println!("{:?}", &x[i][j]);
+        }
+    }
+
+
+
+    let mat: Matrix = mult2d(x, y);
+
+    // for i in mat.data.clone().iter(){  
+    //     println!("{:?}", i);
+    // }
+
+    Ok(atoms::ok().encode(env))
+}
+
+fn nif_fit<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
+    let x: Vec<Vec<Num>> = args[0].decode()?;
+    println!("{:?}", x);
     
-    let ans: Vec<f64> = x.iter().zip(y.iter())
-        .map(|t| t.0 - t.1)
-        .collect();
+    Ok(atoms::ok().encode(env))
+}
+
+fn nif_dot_product<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
+    // Initialize Arguments
+    // Decode to Vector
+    let x: Vec<Num> = args[0].decode()?;
+    let y: Vec<Num> = args[1].decode()?;
+
+    // Return
+    Ok(dot_product(x, y).encode(env))
+}
+
+fn nif_sub<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let x: Vec<Num> = args[0].decode()?;
+    let y: Vec<Num> = args[1].decode()?;
     
-    Ok((atoms::ok(), ans).encode(env))
+    Ok(sub(x, y).encode(env))
 }
 
 // Multiplication for each elements of vector
-fn emult<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
-    let x: Vec<f64> = args[0].decode()?;
-    let y: Vec<f64> = args[1].decode()?;
+fn nif_emult<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+    let x: Vec<Num> = args[0].decode()?;
+    let y: Vec<Num> = args[1].decode()?;
     
-    let ans: Vec<f64> = x.iter().zip(y.iter())
-        .map(|t| t.0 * t.1)
-        .collect();
-    
-    Ok((atoms::ok(), ans).encode(env))
+    Ok(emult(x, y).encode(env))
 }
 
 fn new<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
@@ -89,11 +187,15 @@ fn new<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
     
     let vec : Vec<i64> = (first..end).collect();
 
-    Ok((atoms::ok(), vec).encode(env))
+    Ok(vec.encode(env))
 }
 
 fn zeros<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
-    let len = (args[0]).decode::<(usize)>()?;
+    let len: usize = (args[0]).decode()?;
     let zero_vec = vec![0; len];
     Ok(zero_vec.encode(env))
 }
+
+// fn fit<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+//     // let x Vec=  
+// }
