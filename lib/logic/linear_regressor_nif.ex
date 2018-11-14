d7a841212033ae8711b8c964ee29adbcb1cce1e2
yukq16 <shikine.yui@gmail.com>
2018-11-14 17:41:24 +0900

    added Kernel_reduction()

--- lib/logic/linear_regressor_nif.ex
+++ lib/logic/linear_regressor_nif.ex
@@ -1,7 +1,7 @@
 defmodule LinearRegressorNif do
   use Rustler, otp_app: :linear_regressor_cli, crate: :linear_regressor_nif
 
-  @index 1.024e6
+  @index 1.024e3/2
 
   @doc """
   ## Examples
@@ -14,8 +14,12 @@ defmodule LinearRegressorNif do
   def _new(_a, _b), do: exit(:nif_not_loaded)
   def _sub(_a, _b), do: exit(:nif_not_loaded)
   def _emult(_a, _b), do: exit(:nif_not_loaded)
-  def _call_ocl(_a, _b), do: exit(:nif_not_loaded)
+  def _call_ocl_dp(_a, _b), do: exit(:nif_not_loaded)
+  def _call_ocl_dot(_a, _b), do: exit(:nif_not_loaded)
+  def _call_ocl_nrm(_a), do: exit(:nif_not_loaded)
   def gpuinfo(), do: exit(:nif_not_loaded)
+  def _dot_array(_a, _b), do: exit(:nif_not_loaded)
+  def _norum(_a), do: exit(:nif_not_loaded)
 
   # Main Function
   def _fit(_x, _y, _theta, _alpha, _iteration), do: exit(:nif_not_loaded)
@@ -27,13 +31,53 @@ defmodule LinearRegressorNif do
       a |> to_float,
       b |> to_float
     )
+    receive do
+      l -> l 
+    end
+  end
+
+  def dot_array(a, b)
+      when is_list(a) and is_list(b) do
+    _dot_array(
+      a |> to_float,
+      b |> to_float
+    )
+  end
+
+  def norum(a)
+    when is_list(a) do
+    _norum(a |> to_float)
+    receive do
+      l -> l
+    end
   end
 
   def ocl_dp(a, b)
       when is_list(a) and is_list(b) do
-    _call_ocl(
+    _call_ocl_dp(
       a |> to_float,
       b |> to_float
+    )
+    receive do
+      l -> l 
+    end
+  end
+
+  def ocl_dot(a, b)
+      when is_list(a) and is_list(b) do
+    _call_ocl_dot(
+      a |> to_float,
+      b |> to_float
+    )
+    receive do
+      l -> l 
+    end
+  end
+
+  def ocl_nrm(a)
+      when is_list(a) do
+    _call_ocl_nrm(
+      a |> to_float
     )
     receive do
       l -> l 
@@ -104,6 +148,56 @@ defmodule LinearRegressorNif do
     m = List.duplicate(1, @index |> Kernel.trunc)
     ocl_dp(m, m)
     |>IO.puts
+  end
+
+  def benchmark_rust_dot() do
+    :timer.tc(fn -> test_rust_dot() end)
+    |> elem(0)
+    |> Kernel./(1_000_000)
+    |> IO.puts
+  end
+
+  def benchmark_ocl_dot do
+    :timer.tc(fn -> test_ocl_dot() end)
+    |> elem(0)
+    |> Kernel./(1_000_000)
+    |> IO.puts
+  end
+
+  def test_rust_dot do
+    m = List.duplicate(1, @index |> Kernel.trunc)
+    dot_array(m, m)
+  end
+
+  def test_ocl_dot do
+    m = List.duplicate(1, @index |> Kernel.trunc)
+    ocl_dot(m, m)
+  end
+
+  def benchmark_rust_reduction do
+    :timer.tc(fn -> test_rust_reduction() end)
+    |> elem(0)
+    |> Kernel./(1_000_000)
+    |> IO.puts
+  end
+
+  def test_rust_reduction do
+    m = List.duplicate(1, @index |> Kernel.trunc)
+    norum(m)
+    |> IO.puts
+  end
+
+  def benchmark_ocl_reduction do
+    :timer.tc(fn -> test_ocl_reduction() end)
+    |> elem(0)
+    |> Kernel./(1_000_000)
+    |> IO.puts
+  end
+
+  def test_ocl_reduction do
+    m = List.duplicate(1, @index |> Kernel.trunc)
+    ocl_nrm(m)
+    |> IO.puts
   end
 end
 
--- native/linear_regressor_nif/src/lib.rs
+++ native/linear_regressor_nif/src/lib.rs
@@ -27,13 +27,17 @@ rustler_export_nifs! {
   [
     //("Elixir's func, number of arguments, Rust's func)
     ("_dot_product", 2, nif_dot_product),
+    ("_norum", 1, nif_norum),
+    ("_dot_array", 2, nif_dot_array),
     ("_zeros", 1, zeros),
     ("_new", 2, nif_new),
     ("_sub", 2, nif_sub),
     ("_emult", 2, nif_emult),
     ("_fit", 5, nif_fit), 
-    ("_call_ocl", 2, call_ocl),
     ("gpuinfo", 0, gpuinfo),
+    ("_call_ocl_dp", 2, call_ocl_dp),
+    ("_call_ocl_dot", 2, call_ocl_dot),
+    ("_call_ocl_nrm", 1, call_ocl_nrm),
   ],
   None
 }
@@ -46,6 +50,18 @@ pub fn dot_product(x: &Vec<f64>, y: &Vec<f64>) -> f64 {
   x.iter().zip(y.iter())
   .map(|t| t.0 * t.1)
   .fold(0.0, |sum, i| sum + i)
+}
+
+pub fn norum(x: &Vec<f64>) -> f64 {
+  x.iter()
+  .map(|t| t*t)
+  .fold(0.0, |sum, i| sum + i)
+}
+
+pub fn dot_array(x: &Vec<f64>, y: &Vec<f64>) -> Vec<f64> {
+  x.iter().zip(y.iter())
+  .map(|t| t.0 * t.1)
+  .collect()
 }
 
 pub fn gpuinfo<'a>(env: Env<'a>, _args: &[Term<'a>]) -> NifResult<Term<'a>> {
@@ -84,8 +100,7 @@ pub fn gpuinfo<'a>(env: Env<'a>, _args: &[Term<'a>]) -> NifResult<Term<'a>> {
   Ok(atoms::ok().to_term(env))
 }
 
-
-pub fn call_ocl<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+pub fn call_ocl_dp<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
   use rustler::Error;
 
   let pid = env.pid();
@@ -109,11 +124,91 @@ pub fn call_ocl<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
         let x: Vec<f64> = try!(tuple.0.decode());
         let y: Vec<f64> = try!(tuple.1.decode());
 
-        let result: ocl::Result<(Vec<f64>)> = dot_product_ocl(&x, &y);
+        let result: ocl::Result<(Vec<f64>)> = dot_array_ocl(&x, &y);
         match result {
           Ok(res) => {
-            // println!("{:?}", res);
-            Ok(res.iter().fold(0.0, |sum, i| sum + i).encode(env))
+            Ok(res.encode(env))
+          },
+          Err(_) =>  Err(Error::BadArg),
+        }
+      })();
+      match result {
+          Err(_err) => env.error_tuple("test failed".encode(env)),
+          Ok(term) => term
+      }  
+    });
+  });
+  Ok(atoms::ok().to_term(env))
+}
+
+pub fn call_ocl_dot<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+  use rustler::Error;
+
+  let pid = env.pid();
+  let mut my_env = OwnedEnv::new();
+
+  let saved_list = my_env.run(|env| -> NifResult<SavedTerm> {
+    let _x = args[0].in_env(env);
+    let _y = args[1].in_env(env);
+    Ok(my_env.save(make_tuple(env, &[_x, _y])))
+  })?;
+
+  POOL.spawn(move ||  {
+    my_env.send_and_clear(&pid, |env| {
+      let result: NifResult<Term> = (|| {
+        let tuple = saved_list
+        .load(env).decode::<(
+          Term,
+          Term
+        )>()?; 
+        
+        let x: Vec<f64> = try!(tuple.0.decode());
+        let y: Vec<f64> = try!(tuple.1.decode());
+
+        let result: ocl::Result<(Vec<f64>)> = dot_array_ocl(&x, &y);
+        match result {
+          Ok(res) => {
+            Ok(res.encode(env))
+          },
+          Err(_) =>  Err(Error::BadArg),
+        }
+      })();
+      match result {
+          Err(_err) => env.error_tuple("test failed".encode(env)),
+          Ok(term) => term
+      }  
+    });
+  });
+  Ok(atoms::ok().to_term(env))
+}
+
+pub fn call_ocl_nrm<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
+  use rustler::Error;
+
+  let pid = env.pid();
+  let mut my_env = OwnedEnv::new();
+  let saved_list = my_env.run(|env| -> NifResult<SavedTerm> {
+    let _x = args[0].in_env(env);
+    let _x1 = args[0].in_env(env);
+    Ok(my_env.save(make_tuple(env, &[_x, _x1])))
+  })?;
+
+  std::thread::spawn(move ||  {
+    my_env.send_and_clear(&pid, |env| {
+      let result: NifResult<Term> = (|| {
+        let tuple = saved_list
+        .load(env).decode::<(
+          Term,
+          Term
+        )>()?; 
+        
+        let x: Vec<f64> = try!(tuple.0.decode());
+
+        let result: ocl::Result<(Vec<f64>)> = norum_ocl(&x);
+        match result {
+          Ok(res) => {
+
+            Ok(norum(&res).encode(env))
           },
           Err(_) =>  Err(Error::BadArg),
         }
@@ -135,7 +230,6 @@ pub fn dot_product_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
   let platform = Platform::default();
   let device = Device::first(platform).unwrap(); 
   let f64_size :usize = 8;
-  let kernel_vector_dim :usize = 1;
 
   // 並列化の最大次元数 1 -> ベクトル
   let work_dim : u32 = match device.info(DeviceInfo::MaxWorkItemDimensions){
@@ -188,9 +282,11 @@ pub fn dot_product_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
   // 1つのワークグループにつき処理されるワーク数の最適値
   let work_item_num = (vec_size as u32 + compute_unit_num-1) / compute_unit_num;
   
+
   //let global_work_size = vec_size / kernel_vector_dim work_item;
-  let global_work_size = 256* compute_unit_num;
-  let local_work_size = 256;
+  // let global_work_size = 1024;
+  // let local_work_size = 128;
+  // let group_size = 8;
 
   // ローカルメモリーサイズ
   let local_memory_size = match max_local_memory_size > (vec_size * f64_size) as u32{
@@ -208,12 +304,12 @@ pub fn dot_product_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
   println!("max_local_memory_size:{:?}", max_local_memory_size);
   println!("max_work_item_size:{:?}", max_work_item_size);
   println!("work_item_num:{:?}", work_item_num);
-  println!("global_work_size{:?}", global_work_size);
+  // println!("global_work_size{:?}", global_work_size);
   println!("local_memory_size:{:?}", local_memory_size);
   println!("local_array_size:{:?}", local_array_size);
   
-  println!("local_work_size:{:?}", local_work_size);
-  println!("global_work_size:{:?}", global_work_size);
+  // println!("local_work_size:{:?}", local_work_size);
+  // println!("global_work_size:{:?}", global_work_size);
 
   let src = r#"
     __kernel void dot_product4(
@@ -249,43 +345,34 @@ pub fn dot_product_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
       }
     }
 
-    __kernel void dot_product1(
-      __global double* x, 
-      __global double* y,
-      __global double* output,
-      int array_size,
-      __local double* partial_sums
-    ){
-      int lid = get_local_id(0);
-      int gid = get_global_id(0);
-      int offset = get_local_size(0);
+    // __kernel void reduction(
+    //   __global double* vec,
+    //   __local double* out, 
+    //   double* partial_sums
+    // ){
+    //   partial_sums[lid] = x[gid] * y[gid];
 
+    //   for(int i = offset >> 1; i > 0; i >>= 1) {
+    //       barrier(CLK_LOCAL_MEM_FENCE);
 
-      if(gid >= array_size){
-        return;
-      }
+    //       if(lid < i) {
+    //           partial_sums[lid] += partial_sums[lid + i];
+    //       }
+    //   }
 
-      // printf("group_size:%d\n", offset);
-      // printf("x*y[%d] = %lf\n", gid, x[gid]*y[gid]);
+    //   // if(lid == 0) {
+    //   //   output[get_group_id(0)] = partial_sums[0];
+    //   // }
+    // }
 
-      partial_sums[lid] = x[gid] * y[gid];
-
-      // printf("partial_sums[%d]= %lf\n", lid, partial_sums[lid]);
-
-      for(int i = offset >> 1; i > 0; i >>= 1) {
-          barrier(CLK_LOCAL_MEM_FENCE);
-
-          if(lid < i) {
-              partial_sums[lid] += partial_sums[lid + i];
-          }
-      }
-
-      // printf("group_id[%d]\n", get_group_id(0));
-
-      if(lid == 0) {
-        output[get_group_id(0)] = partial_sums[0];
-      }
-    }
+    // __kernel void dot_product1(
+    //   __global double* x, 
+    //   __global double* y,
+    //   __global double* output,
+    //   __local double* partial_sums
+    // ){
+    //   dot(x, y, output);
+    // }
   "#;
 
   let pro_que = ProQue::builder()
@@ -316,12 +403,14 @@ pub fn dot_product_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
 
   let kernel : ocl::Kernel = 
     pro_que.kernel_builder(
-      ["dot_product", &kernel_vector_dim.to_string()].concat())
+      //["dot_product", &kernel_vector_dim.to_string()].concat()
+      "dot_array"
+      )
     .arg(&source_buffer_x) 
     .arg(&source_buffer_y)
     .arg(&output_buffer)
-    .arg(&local_array_size)
-    .arg_local::<f64>(local_array_size)
+    .arg(&vec_size)
+    // .arg_local::<f64>(1024)
     .build()?;
 
   // OpenCL Debug
@@ -332,17 +421,201 @@ pub fn dot_product_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
     res = kernel.cmd()
       .queue(pro_que.queue())
       .global_work_offset(kernel.default_global_work_offset())
-      .global_work_size(global_work_size)
-      .local_work_size(kernel.default_local_work_size())
-      // .local_work_size(local_work_size)
+      .global_work_size(vec_size)
+      .local_work_size(4)
       .enq();
   }
 
+  // let group_size = vec_size / 4;
 
   match res {
     Ok(_) => {
       println!("success execute kernel code");
       let mut result = vec![0f64; vec_size];
+      output_buffer.read(&mut result).enq()?;
+      Ok(result)
+    },
+    Err(err) => {
+      println!("{:?}", err);
+      Err(err)
+    },
+  }
+}
+
+pub fn dot_array_ocl(x: &Vec<f64>, y: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
+  let vec_size = x.len();
+
+  println!("input_size:{:?}", vec_size);
+
+  let src = r#"
+    __kernel void dot_array(
+      __global double* x, 
+      __global double* y,
+      __global double* output,
+      int vec_size
+    ){
+      int gid = get_global_id(0);
+      
+      if(gid >= vec_size){
+        return;
+      }
+
+      output[gid] = x[gid]*y[gid];
+    }
+  "#;
+
+  let pro_que = ProQue::builder()
+    .src(src)
+    .dims(1)
+    .build().expect("Build ProQue");
+
+  let source_buffer_x = Buffer::builder()
+    .queue(pro_que.queue().clone())
+    .flags(MemFlags::new().read_write())
+    .len(vec_size)
+    .copy_host_slice(&x)
+    .build()?;
+
+  let source_buffer_y = Buffer::builder()
+    .queue(pro_que.queue().clone())
+    .flags(MemFlags::new().read_write())
+    .len(vec_size)
+    .copy_host_slice(&y)
+    .build()?;
+
+  let output_buffer = Buffer::<f64>::builder()
+    .queue(pro_que.queue().clone())
+    .flags(MemFlags::new().write_only())
+    .len(vec_size)
+    .fill_val(0f64)
+    .build()?;
+
+  let kernel : ocl::Kernel = 
+    pro_que.kernel_builder("dot_array")
+    .arg(&source_buffer_x) 
+    .arg(&source_buffer_y)
+    .arg(&output_buffer)
+    .arg(&vec_size)
+    .build()?;
+
+  let res: ocl::Result<()>;
+  unsafe { 
+    res = kernel.cmd()
+      .queue(pro_que.queue())
+      .global_work_offset(kernel.default_global_work_offset())
+      .global_work_size(vec_size)
+      .local_work_size(4)
+      .enq();
+  }
+
+  match res {
+    Ok(_) => {
+      println!("success execute kernel code");
+      let mut result = vec![0f64; vec_size];
+      output_buffer.read(&mut result).enq()?;
+      Ok(result)
+    },
+    Err(err) => {
+      println!("{:?}", err);
+      Err(err)
+    },
+  }
+}
+
+pub fn norum_ocl(x: &Vec<f64>) -> ocl::Result<(Vec<f64>)> {
+  use ocl::{Platform, Device};
+  use ocl::enums::{DeviceInfo, DeviceInfoResult};
+
+  let vec_size = x.len();
+  let platform = Platform::default();
+  let device = Device::first(platform).unwrap(); 
+
+  // ワークグループあたりのワークアイテム数？
+  let max_work_item_sizes: Vec<usize> = match device.info(DeviceInfo::MaxWorkItemSizes){
+    Ok(DeviceInfoResult::MaxWorkItemSizes(res)) => res,
+    _ => { 
+      println!("failed to get DeviceInfoResult::MaxWorkGroupSize");
+      vec![1; 3]
+    },
+  };
+
+  println!("input_size:{:?}", vec_size);
+  println!("max_work_item_sizes:{:?}", max_work_item_sizes);
+
+  let src = r#"
+    __kernel void reduction(
+      __global const double* vec,
+      __global double* partial_sums, 
+      __local double* local_sums
+    ){
+      uint lid = get_local_id(0);
+      uint group_size = get_local_size(0);
+      
+      local_sums[lid] = vec[get_global_id(0)];
+
+      // Loop for computing localSums : divide WorkGroup into 2 parts
+      for (uint stride = group_size/2; stride > 0; stride /=2)
+      {
+        // Waiting for each 2x2 addition into given workgroup
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // Add elements 2 by 2 between local_id and local_id + stride
+        if (lid < stride)
+          local_sums[lid] += local_sums[lid + stride];
+      }
+
+      // Write result into partialSums[nWorkGroups]
+      if (lid == 0)
+        partial_sums[get_group_id(0)] = local_sums[0]; 
+    }
+  "#;
+
+  let pro_que = ProQue::builder()
+    .src(src)
+    .dims(1)
+    .build().expect("Build ProQue");
+
+  let source_buffer_x = Buffer::builder()
+    .queue(pro_que.queue().clone())
+    .flags(MemFlags::new().read_write())
+    .len(vec_size)
+    .copy_host_slice(&x)
+    .build()?;
+
+  let work_group_size = 1;
+  let work_group_num = vec_size / work_group_size;
+  println!("work_group_size:{:?}", work_group_size);
+  println!("work_group_num:{:?}", work_group_num);
+
+  let output_buffer = Buffer::<f64>::builder()
+    .queue(pro_que.queue().clone())
+    .flags(MemFlags::new().write_only())
+    .len(work_group_num)
+    .fill_val(0f64)
+    .build()?;
+
+
+  let kernel : ocl::Kernel = 
+    pro_que.kernel_builder("reduction")
+    .arg(&source_buffer_x) 
+    .arg(&output_buffer)
+    .arg_local::<f64>(work_group_size)
+    .build()?;
+
+  let res: ocl::Result<()>;
+  unsafe { 
+    res = kernel.cmd()
+      .queue(pro_que.queue())
+      .global_work_offset(kernel.default_global_work_offset())
+      .global_work_size(vec_size)
+      .local_work_size(work_group_size)
+      .enq();
+  }
+
+  match res {
+    Ok(_) => {
+      println!("success execute kernel code");
+      let mut result = vec![0f64; work_group_num];
       output_buffer.read(&mut result).enq()?;
       Ok(result)
     },
@@ -456,10 +729,79 @@ fn nif_fit<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
 }
 
 fn nif_dot_product<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
+  let pid = env.pid();
+  let mut my_env = OwnedEnv::new();
+
+  let saved_list = my_env.run(|env| -> NifResult<SavedTerm> {
+    let _x = args[0].in_env(env);
+    let _y = args[1].in_env(env);
+    Ok(my_env.save(make_tuple(env, &[_x, _y])))
+  })?;
+
+  std::thread::spawn(move ||  {
+    my_env.send_and_clear(&pid, |env| {
+      let result: NifResult<Term> = (|| {
+        let tuple = saved_list
+        .load(env).decode::<(
+          Term,
+          Term
+        )>()?; 
+        
+        let x: Vec<f64> = try!(tuple.0.decode());
+        let y: Vec<f64> = try!(tuple.1.decode());
+
+        Ok(dot_product(&x, &y).encode(env))
+      })();
+      match result {
+          Err(_err) => env.error_tuple("test failed".encode(env)),
+          Ok(term) => term
+      }  
+    });
+  });
+  Ok(atoms::ok().to_term(env))
+}
+
+fn nif_norum<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
+  // let x :Vec<f64> = try!(args[0].decode());
+  // Ok(norum(&x).encode(env))
+
+  // println!("{:?}", x);
+
+  let pid = env.pid();
+  let mut my_env = OwnedEnv::new();
+  let saved_list = my_env.run(|env| -> NifResult<SavedTerm> {
+    let _x = args[0].in_env(env);
+    let _x1 = args[0].in_env(env);
+    Ok(my_env.save(make_tuple(env, &[_x, _x1])))
+  })?;
+
+  std::thread::spawn(move ||  {
+    my_env.send_and_clear(&pid, |env| {
+      let result: NifResult<Term> = (|| {
+        let tuple = saved_list
+        .load(env).decode::<(
+          Term,
+          Term
+        )>()?; 
+        
+        let x: Vec<f64> = try!(tuple.0.decode());
+
+        Ok(norum(&x).encode(env))
+      })();
+      match result {
+          Err(_err) => env.error_tuple("test failed".encode(env)),
+          Ok(term) => term
+      }  
+    });
+  });
+  Ok(atoms::ok().to_term(env))
+}
+
+fn nif_dot_array<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
   let x: Vec<f64> = args[0].decode()?;
   let y: Vec<f64> = args[1].decode()?;
 
-  Ok(dot_product(&x, &y).encode(env))
+  Ok(dot_array(&x, &y).encode(env))
 }
 
 fn nif_sub<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
