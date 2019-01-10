78418b5755f9199ef257d759a3745d2149175bc1
yukq16 <shikine.yui@gmail.com>
2019-01-10 12:38:49 +0900

    improve new func to async func for vector

--- native/linear_regressor_nif/src/matrix/single_core/nif.rs
+++ native/linear_regressor_nif/src/matrix/single_core/nif.rs
@@ -1,15 +1,10 @@
-
 use rustler::{Env, Term, NifResult, Encoder};
-
 use matrix::single_core as sc;
 
 pub fn dot_product<'a>(env: Env<'a>, args: &[Term<'a>])-> NifResult<Term<'a>> {
-  // Initialize Arguments
-  // Decode to Vector
   let x: Vec<f64> = args[0].decode()?;
   let y: Vec<f64> = args[1].decode()?;
 
-  // Return
   Ok(sc::dot_product(&x, &y).encode(env))
 }
 
@@ -28,10 +23,37 @@ pub fn emult<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
 }
 
 pub fn new<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
-  let first: i64 = args[0].decode()?;
-  let end: i64 = args[1].decode()?;
+  let pid = env.pid();
+  let mut my_env = OwnedEnv::new();
 
-  Ok(sc::new(first, end).encode(env))
+  let saved_list = my_env.run(|env| -> NifResult<SavedTerm> {
+    let _a = args[0].in_env(env);
+    let _b = args[1].in_env(env);
+    Ok(my_env.save(make_tuple(env, &[_a, _b])))
+  })?;
+
+  std::thread::spawn(move ||  {
+    my_env.send_and_clear(&pid, |env| {
+      let result: NifResult<Term> = (|| {
+        let tuple = saved_list
+        .load(env).decode::<(  
+          i64,
+          i64)>()?; 
+        
+        let first: i64 = tuple.0.decode()?;
+        let end: i64 = tuple.1.decode()?;
+
+        let ans = sc::new(first, end);
+
+        Ok(ans.encode(env))
+      })();
+      match result {
+          Err(_err) => env.error_tuple("test failed".encode(env)),
+          Ok(term) => term
+      }  
+    });
+  });
+  Ok(atoms::ok().to_term(env))
 }
 
 pub fn zeros<'a>(env: Env<'a>, args: &[Term<'a>]) -> NifResult<Term<'a>> {
