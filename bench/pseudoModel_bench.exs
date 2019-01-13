c0a9a90454c82c5e36eaca728b2dbb812d08e643
yukq16 <shikine.yui@gmail.com>
2019-01-13 09:53:11 +0900

    add benchmarks

--- bench/pseudoModel_bench.exs
+++ bench/pseudoModel_bench.exs
@@ -1,27 +1,7 @@
 defmodule PseudoModelBench do
   use Benchfella
 
-  bench "Rust", [data: setup()] do
-    {x_train, y_train, alpha, iterations} = data
-
-    LinearRegressorNif.SingleCore.fit( 
-      x_train , 
-      y_train , 
-      alpha, 
-      iterations )
-  end
-
-  bench "Rayon", [data: setup()] do
-    {x_train, y_train, alpha, iterations} = data
-
-    LinearRegressorNif.MultiCore.fit( 
-      x_train , 
-      y_train , 
-      alpha, 
-      iterations )
-  end
-
-  defp setup do
-    LinearModel.setup 10, 5000
+  bench "Speed up efficiency", [] do
+    LinearModel.all_benchmark 10
   end
 end
