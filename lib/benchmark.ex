6a405ff1be35baefb5eb0249c95c210cde2d6f28
yukq16 <shikine.yui@gmail.com>
2019-01-13 09:21:35 +0900

    don’t inspect benchmark

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -2,8 +2,8 @@ defmodule Benchmark do
   defmacro time(exp, tmp \\ false) do
     quote do
       {time, dict} = :timer.tc(fn() -> unquote(exp) end)
-      IO.inspect "time: #{time} micro second"
-      IO.inspect "-------------"
+      # IO.inspect "time: #{time} micro second"
+      # IO.inspect "-------------"
       case unquote(tmp) do
         false -> dict
         true -> {time, dict}
